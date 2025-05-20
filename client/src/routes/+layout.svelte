31003bca369cfe11b7594d792802c38f3c2e8760
Hanna Hummal <hannahummal@Hannas-MacBook-Pro.local>
2025-05-20 21:51:47 +0300

    layout, global styles, project structure

---
+++ client/src/routes/+layout.svelte
@@ -0,0 +1,23 @@
+<script>
+/** Content **/
+</script>
+
+<header>
+    <nav>
+        <a href="/">Home</a>
+    </nav>
+</header>
+<main>
+    
+    <h1>
+        <p>Welcome to Ilus.me</p>
+    </h1>
+    <!-- Content -->
+</main>
+<footer>
+    <nav>
+        <a href="/">Conscious Beauty</a>
+      </nav>
+</footer>
+
+  
