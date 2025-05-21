8fb0803d76dfefaf6549e9dcf81032b2c1a60ce8
Hanna Hummal <hannahummal@Hannas-MacBook-Pro.local>
2025-05-21 19:56:01 +0300

    placeholder product cards, dummy data

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -9,3 +9,29 @@
     <button>🔍</button>
   </div>
 </section>
+
+<section class="product-preview">
+  <h2>Recently Added Products</h2>
+  <div class="product-grid">
+    <div class="product-card">
+      <img src="/photos/carousel1.jpg" alt="Product Image" />
+      <h3>Sample</h3>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque, obcaecati laboriosam. Odit totam autem dignissimos dolores doloribus mollitia libero? Ut, dolorem praesentium tempore labore enim culpa. Est voluptatibus iste itaque.</p>
+      <button>View</button>
+    </div>
+
+    <div class="product-card">
+      <img src="/photos/carousel1.jpg" alt="Product Image" />
+      <h3>Sample</h3>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam, reprehenderit? Fugit illo quam quae recusandae numquam, iste, corrupti voluptate alias mollitia dicta eius voluptatem autem sapiente odit esse molestias. Officiis?</p>
+      <button>View</button>
+    </div>
+
+    <div class="product-card">
+      <img src="/photos/carousel1.jpg" alt="Product Image" />
+      <h3>Sample</h3>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam, reprehenderit? Fugit illo quam quae recusandae numquam, iste, corrupti voluptate alias mollitia dicta eius voluptatem autem sapiente odit esse molestias. Officiis?</p>
+      <button>View</button>
+    </div>
+  </div>
+</section>
