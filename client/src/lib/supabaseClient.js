726c78360f6cf7bc09c1eeacae08530d4c7f2599
Hanna Hummal <hannahummal@Hannas-MacBook-Pro.local>
2025-05-22 14:07:10 +0300

    sign up bug fix and testing

--- client/src/lib/supabaseClient.js
+++ client/src/lib/supabaseClient.js
@@ -1,6 +1,6 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://szpkjpsgadcbitfeewwv.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN6cGtqcHNnYWRjYml0ZmVld3d2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY2ODQyMjIsImV4cCI6MjA2MjI2MDIyMn0.XUyas6QeWx2V-cDnp2nStF69p6vh8TROnMcrINQTVvo'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
