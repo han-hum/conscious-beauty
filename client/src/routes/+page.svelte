7f7ed95efcb42105d5821039eda89238ca108a21
Hanna Hummal <hannahummal@Hannas-MBP.lan>
2025-05-23 12:35:52 +0300

    Read real product data from Supabase

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -2,22 +2,17 @@
   import { supabase } from '$lib/supabaseClient';
   import { onMount } from 'svelte';
 
-let products = [];
+  let products = []; /* */
 
-onMount(async () => {  /*!CORS is used just for development fetch and will be replaced later!*/
-  const response = await fetch('https://corsproxy.io/?https://world.openbeautyfacts.org/category/cosmetics.json');
+onMount(async () => {
+  const { data, error } = await supabase.from('products').select('*');
+  if (error) {
+    console.error('Error fetching products:', error.message);
+  } else {
+    products = data;
+    console.log('Fetched products:', products);
 
-  const json = await response.json();
-
-  //fetch 10 example products
-  products = (json.products || [])
-    .filter(p => p.product_name && p.image_url)
-    .slice(0, 10)
-    .map(p => ({
-      name: p.product_name,
-      description: p.brands || 'No brand info',
-      image_url: p.image_url
-    }));
+  }
 });
 
 </script>
@@ -46,38 +41,3 @@ onMount(async () => {  /*!CORS is used just for development fetch and will be re
     {/each}
   </div>
 </section>
-
-
-<!--
-<section class="product-preview">
-
-  <h2>Recently Added Products</h2>
-
-  <div class="product-grid">
-
-    {#each products as product}
-    <div class="product-card">
-      <img src={product.image_url} alt={product.name} />
-      <h3>{product.name}</h3>
-      <p>{product.description}</p>
-      <button>View</button>
-    </div>
-
-    <div class="product-card">
-      <img src={product.image_url} alt={product.name} />
-      <h3>{product.name}</h3>
-      <p>{product.description}</p>
-      <button>View</button>
-    </div>
-
-    <div class="product-card">
-      <img src={product.image_url} alt={product.name} />
-      <h3>{product.name}</h3>
-      <p>{product.description}</p>
-      <button>View</button>
-    </div>
-
-    {/each}
-
-</section>
--->
